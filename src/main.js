804f5aa0d17473fa77609298f37bb5df21c84983
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2025-01-15 01:26:36 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
-const scripts = [];
+const scripts = []; // Массив для хранения функций из модулей
 
+// Импортируем все модули из папки modules
 const modules = import.meta.glob("./modules/*.js");
 
+// Асинхронная функция для загрузки модулей
 async function loadModules() {
+  // Перебираем все модули
   for (const modulePath in modules) {
+    // Импортируем модуль
     const module = await modules[modulePath]();
 
     // Перебираем все функции в модуле
     Object.values(module).forEach((func) => {
+      // Проверяем, является ли функция
       if (typeof func === "function") {
+        // Добавляем функцию в массив
         scripts.push(func);
       }
     });
   }
-
+  // Выполняем все функции из массива
   scripts.forEach((script) => script());
 }
 
+// Вызываем функцию загрузки модулей
 loadModules();
 
 const obj = {
